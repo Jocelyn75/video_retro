a9057173b059cc24e89ab3cd2725d19d6a843f6a
Jocelyn Dufour <jocelyn.dufour@outlook.com>
2024-04-20 22:55:38 +0200

    Ajout de commentaires

--- src/Service/TMDBService.php
+++ src/Service/TMDBService.php
@@ -13,6 +13,7 @@ class TMDBService
         $this->client = $client;
     }
 
+    // Requête pour la barre de recherche : recherche d'un film par mots-clés.
     public function searchFilms(string $searchQuery): array
     {
         $apiResponse = $this->client->request('GET', "https://api.themoviedb.org/3/search/movie?query={$searchQuery}&language=fr&api_key={$_ENV['TMDB_API']}");
@@ -20,18 +21,21 @@ class TMDBService
         return $apiResponseArray['results'];
     }
 
+    // Requête pour la fiche film : récupération des données d'un film à partir de son id
     public function getFilmDetails(string $id): array
     {
         $apiResponse = $this->client->request('GET', "https://api.themoviedb.org/3/movie/{$id}?&language=fr&api_key={$_ENV['TMDB_API']}");
         return $apiResponse->toArray();
     }
 
+    // Requête pour la fiche film : récupérer les crédits d'un film à partir de son id
     public function getFilmCredits(string $id): array
     {
         $apiResponse = $this->client->request('GET', "https://api.themoviedb.org/3/movie/{$id}/credits?language=fr&api_key={$_ENV['TMDB_API']}");
         return $apiResponse->toArray();
     }
 
+    // Requête pour la fiche film : récupérer les services de VOD et SVOD sur lesquels le film est disponible, à partir de l'id du film
     public function getFilmProviders(string $id): array
     {
         $apiResponse = $this->client->request('GET', "https://api.themoviedb.org/3/movie/{$id}/watch/providers?language=fr&api_key={$_ENV['TMDB_API']}");
