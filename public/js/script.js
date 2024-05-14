353a129a38926469dc7bd273153f8b0055b48777
Jocelyn Dufour <jocelyn.dufour@outlook.com>
2024-05-14 20:59:05 +0200

    Ajustements mineurs

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+// Set timeout pour les messages flash
+
+// setTimeout(function() {
+//     let flashMessage = document.getElementById('flash-message');
+//     if (flashMessage) {
+//         flashMessage.style.display = 'none';
+//     }
+// }, 5000);
